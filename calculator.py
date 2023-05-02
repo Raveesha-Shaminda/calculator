3047b559a42d4129dd44418ebe40ce8982c80301
RAVEESHA SHAMINDA <96763137+Raveesha-Shaminda@users.noreply.github.com>
2023-05-03 01:17:36 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+#I'm Raveesha Shaminda, Big Python Program(calculator)..
+#✅
+#wa.me/+94715248393 ✅ 
+
 num1 = int(input("Enter The Number : "))
 str = str(input("Enter [+,-,*,/] : "))
 num2 = int(input("Enter The Number : "))
@@ -12,3 +16,12 @@ elif str == "/":
     output = num1 / num2
 
 print(f"Answer is :{output}")
+
+
+"""
++ -- ✅ 
+- -- ✅ 
+* -- ✅ 
+/ -- ✅
+float -- 🚫
+"""
